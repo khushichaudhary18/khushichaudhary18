dcebc7eb10895bd2e87568aed74949d795531edc
khushee_06 <khushichaudhary2063@gmail.com>
2026-04-18 12:34:22 +0545

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+current_date=$(date +%Y-%m-%d)
+
+readme=$(cat README.md)
+
+new_readme=$(echo "$readme" | awk -v new_date="$current_date" '
+  /date:/ { $0="date: " new_date }
+  1
+')
+
+echo "$new_readme" > README.md
+
+git add README.md
+git diff --quiet || git commit -m "Auto update date"
+git push origin main
